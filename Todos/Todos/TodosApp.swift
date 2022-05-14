a2ec6f6fede61e94032bc031e5d4b7132ac1b7ef
Maxim Sidorov <s_maxim_v@bk.ru>
2022-05-14 20:39:39 +0300

    add todos project

---
+++ Todos/Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by MSP on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
